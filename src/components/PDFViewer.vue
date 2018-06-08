82db8b2709cc80e18681221f246e3d5bac1d2308
Forever <lynk0613@hotmail.com>
2018-06-08 11:20:48 -0400

    get info

--- src/components/PDFViewer.vue
+++ src/components/PDFViewer.vue
@@ -1,9 +1,37 @@
 <template>
   <div class="hello">
-		<pdf
-			ref="myPdfComponent"
-			src="./static/1.pdf"
-		></pdf>
+    <button @click="logContent()">
+			getInfo
+		</button>
+    <div class="preview-pdf">
+      <h2>fn</h2>
+		  <pdf ref="myPdfComponent" :src="fn"></pdf>
+    </div>
+    <div class="result-data">
+      <h2>Result</h2>
+      <table>
+        <tr>
+          <th>Date</th>
+          <th>Value date</th>
+          <th>Ref. number</th>
+          <th>Trans. Type</th>
+          <th>Description</th>
+          <th>Currency</th>
+          <th>Debit</th>
+          <th>Balance</th>
+        </tr>
+        <tr v-for="(row, index) in data" v-bind:key="index">
+          <td>{{row.date}}</td>
+          <td>{{row.valueDate}}</td>
+          <td>{{row.refNumber}}</td>
+          <td>{{row.transType}}</td>
+          <td>{{row.description}}</td>
+          <td>{{row.currency}}</td>
+          <td>{{row.debit}}</td>
+          <td>{{row.balance}}</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -11,13 +39,15 @@
 import pdf from 'vue-pdf'
 
 export default {
+  /* eslint-disable */
   name: 'PDFViewer',
   components: {
     pdf
   },
   data () {
     return {
-
+      fn: "./static/4.pdf",
+      data: []
     }
   },
 
@@ -27,12 +57,66 @@ export default {
   methods: {
     load: function () {
 
-    }
+    },
+
+    pushArray: function (rowData) {
+      console.log(rowData)
+      this.data.push(rowData)
+    },
+
+    logContent: function () {
+      const vm = this
+      this.$refs.myPdfComponent.pdf.forEachPage ( function (page) {
+        return page.getTextContent()
+        .then (function(content) {
+          const text = content.items.map(item => item.str)
+          const lenthOfWord = text.length
+         
+          for (let i = 0; i < lenthOfWord - 10; i++) {
+            if (text[i] == 'Date' && 
+              text[ i + 1 ] == 'Value date'
+            ) {
+              let j = 9
+              let rowData 
+              for (;;) {
+                rowData = {}  
+                rowData.date = text [ i + j++ ].trim()
+                if (rowData.date.length !== 10) break
+                rowData.valueDate = text [ i + j++ ]
+                if (rowData.valueDate.length !== 10) break
+                rowData.refNumber = text [ i + j++ ]
+                rowData.transType = text [ i + j++ ]
+
+                rowData.description = text [ i + j++ ]
+                while (text [i + j].trim() !=='EUR' && text [i + j].trim() !=='USD') {
+                  rowData.description += ' ' + text [ i + j++ ]
+                }
+           
+                rowData.currency = text [ i + j++ ]
+                rowData.debit = text [ i + j++ ]
+                rowData.credit = text [ i + j++ ]
+                rowData.balance = text [ i + j++ ]
+                vm.pushArray(rowData)
+              }
+              i += j
+            }
+          }
+        })
+			})
+		}
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .preview-pdf {
+    overflow-y: scroll;
+    height: 400px;
+    border: 1px solid;
+  }
+  table, th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+  }
 </style>
